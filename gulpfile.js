1dd01c383d723aea5a1b08b82798a52813e3fff1
andrey <andrey.chaluy.work@gmail.com>
2018-12-13 22:26:09 +0200

    add header,main

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp         = require('gulp'),
+	sass         = require('gulp-sass'),
+	browserSync  = require('browser-sync'),
+	cssnano      = require('gulp-cssnano'),
+	rename       = require('gulp-rename'),
+	imagemin     = require('gulp-imagemin'),
+	pngquant     = require('imagemin-pngquant'),
+	cache        = require('gulp-cache'),
+	autoprefixer = require('gulp-autoprefixer'),
+	gulp         = require('gulp'),
+	babel        = require('gulp-babel'),
+    notify       = require('gulp-notify');
+
+// Tasks 
+
+gulp.task('sass', function() {
+	return  gulp.src('scss/**/*.scss')
+            .pipe(sass().on('error', notify.onError({
+                message: '<%= error.message%>',
+                title: 'Sass Error!'
+            })))
+			.pipe(autoprefixer(['last 15 version', '>1%', 'ie 8', 'ie 7'], {cascade:true}))
+			.pipe(gulp.dest('src/css'))
+});
+
+gulp.task('style-min', function() {
+	return gulp.src([
+		'src/css/**/*.css'])
+		.pipe(cssnano())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({stream:true}))
+});
+
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {baseDir: ''},
+		notify:false
+	});	
+});
+
+
+gulp.task('clear', function() {
+	return cache.clearAll();
+});
+
+gulp.task('img',function() {
+	return gulp.src('img/**/*')
+	.pipe(cache (imagemin({
+		interlaced: true,
+		progressive: true,
+		svgoPlugins:[{removeViewBox:false}],
+		une:[pngquant()]
+	})))
+	.pipe(gulp.dest('img'));
+});
+
+gulp.task('default', ['browser-sync', 'style-min'], function() {
+	gulp.watch('scss/**/*.scss', ['sass']);
+	gulp.watch('src/css/**/*.css', ['style-min']);
+	gulp.watch('src/js/**/*.js', ['scripts']);
+	gulp.watch('*.html', browserSync.reload);
+	gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/js/common.js
@@ -0,0 +1,10 @@
+/*function Function_burger() {
+    var x = document.getElementById("menu");
+    if (x.className === "burger") {
+        x.className += "responsive";
+    } else {
+        x.className = "burger"
+    }
+
+}
+*/
